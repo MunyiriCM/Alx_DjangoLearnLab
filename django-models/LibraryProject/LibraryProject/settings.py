932b2ec2fd47845159cb7822f7d0abad3d6c499b
MunyiriCM <mmunyiri12@gmail.com>
2025-07-19 11:51:31 +0300

    Part 4 manage user authentication

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+STATICFILES_DIRS = [ BASE_DIR / "relationship_app/static" ]
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,3 +14,23 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
 ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    
+    # other views like book_list and library_detail
+]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+]
