586b8ce6be73e10af4df37481945007055be1ae9
MunyiriCM <mmunyiri12@gmail.com>
2025-07-19 11:54:00 +0300

    Part 4 manage user authenticationadjustment

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 STATICFILES_DIRS = [ BASE_DIR / "relationship_app/static" ]
