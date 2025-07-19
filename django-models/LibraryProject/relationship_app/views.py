c59886b4ceb5483a2ad7e5b9da4cf9f0e013c17a
MunyiriCM <mmunyiri12@gmail.com>
2025-07-19 11:01:36 +0300

    part 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
-
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Function-based view to list all books
+# Function-based view
 def list_books(request):
-    books = Book.objects.all('author').all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
 # Create your views here.
 from django.views.generic.detail import DetailView
@@ -14,5 +15,9 @@ from .models import Library
 # Class-based view to show library details and books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
+
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
