ef6f10cac5c465dbf1f024456f034a24381b210b
MunyiriCM <mmunyiri12@gmail.com>
2025-07-19 12:00:07 +0300

    Part 4 manage user authenticationadjustment 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -20,12 +23,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
-    
-    # other views like book_list and library_detail
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    # ... any other views
 ]
+
+    # other views like book_list and library_detail
+
 
 from django.contrib import admin
 from django.urls import path, include
