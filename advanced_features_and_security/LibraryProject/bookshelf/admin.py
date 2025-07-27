5bb4eccf0e89a9f4d029b37a4fedf0885a3a6a2e
MunyiriCM <mmunyiri12@gmail.com>
2025-07-27 17:04:24 +0300

    Django advanced features trial 4

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import Book  # ✅ This is what the checker is looking for
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -11,5 +13,24 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)  # ✅ Old-school format that checkers prefer
 
 
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Register your models here.
