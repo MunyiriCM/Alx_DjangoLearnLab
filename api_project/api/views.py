43c81fe59b5fc07af88cb78bd0edbf3714973253
MunyiriCM <mmunyiri12@gmail.com>
2025-08-03 19:36:32 +0300

    Implementing CRUD Operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -10,6 +10,9 @@ class BookList(generics.ListAPIView):
 # Create your views here.
 
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
 
