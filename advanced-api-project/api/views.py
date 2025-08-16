99229590d411a4522ed7873fe2b241861aa9708c
MunyiriCM <mmunyiri12@gmail.com>
2025-08-16 11:10:03 +0300

    Advanced API

---
+++ advanced-api-project/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+class AuthorListCreateView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Create your views here.
