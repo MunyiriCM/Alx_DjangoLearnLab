ab6a7d3938a2f562d158549fe1dbfd9844881125
MunyiriCM <mmunyiri12@gmail.com>
2025-08-20 22:17:33 +0300

    SOcial media API-Project Setup and User Authentication

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_set",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+# Create your models here.
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from .models import User
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"user": UserSerializer(user).data, "token": token.key})
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"user": UserSerializer(user).data, "token": token.key})
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+# Create your views here.
