a3e9a37f0ba073f2b04c8fcc29c291f2c0220249
MunyiriCM <mmunyiri12@gmail.com>
2025-08-21 12:51:04 +0300

    Implementing Advanced Features: Tagging and Search Functionality 2

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import Comment
 from .models import Post, Tag
+from taggit.forms import TagWidget   # ✅ Import TagWidget
 
 
 class CommentForm(forms.ModelForm):
@@ -23,8 +24,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # ✅ include tags
+        widgets = {
+            'tags': TagWidget(),  # ✅ Use TagWidget for better UI
+        }
 
-        
+
     def save(self, commit=True):
         post = super().save(commit=False)
         if commit:
